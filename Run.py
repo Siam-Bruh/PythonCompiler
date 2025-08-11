d907b003c6f1255a454a33297b92b7bdef6f1221
SiamBhau <smd440046@gmail.com>
2025-08-11 21:23:48 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,13 +1,10 @@
 import PythonCompiler
 
-
-print("Available functions/objects in PythonCompiler)
+print("Available functions/objects in PythonCompiler")
 print(dir(PythonCompiler))
-
 
 if hasattr(PythonCompiler, "main"):
     PythonCompiler.main()
-
 
 elif hasattr(PythonCompiler, "start"):
     PythonCompiler.start()
