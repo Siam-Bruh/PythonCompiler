7c591368e8b16397b2f89429fdd9f9fb93a985e0
SiamBhau <smd440046@gmail.com>
2025-08-11 21:20:54 +0600

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,13 @@
+import PythonCompiler
+
+
+print("Available functions/objects in PythonCompiler)
+print(dir(PythonCompiler))
+
+
+if hasattr(PythonCompiler, "main"):
+    PythonCompiler.main()
+
+
+elif hasattr(PythonCompiler, "start"):
+    PythonCompiler.start()
