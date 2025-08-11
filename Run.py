94947c80c949a1f5e64c7c3931a6ffe3c9c8cefe
SiamBhau <smd440046@gmail.com>
2025-08-11 21:26:32 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,10 +1,11 @@
 import PythonCompiler
 
-print("Available functions/objects in PythonCompiler")
+print("Functions in PythonCompiler:")
 print(dir(PythonCompiler))
 
 if hasattr(PythonCompiler, "main"):
     PythonCompiler.main()
-
 elif hasattr(PythonCompiler, "start"):
     PythonCompiler.start()
+else:
+    print("No main/start function found.")
